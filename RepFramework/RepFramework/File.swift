c3b9fd75040eb60bd72e5d2e459d71d721f8d021
abhishek <abhishek.kumar+wynk@wynk.in>
2024-08-21 14:57:35 +0530

    New Framework added

---
+++ RepFramework/RepFramework/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  RepFramework
+//
+//  Created by B0276971 on 21/08/24.
+//
+
+import Foundation
---
+++ RepFramework/RepFramework/RepMain.swift
@@ -0,0 +1,23 @@
+//
+//  RepMain.swift
+//  RepFramework
+//
+//  Created by B0276971 on 21/08/24.
+//
+
+import Foundation
+
+public class RepMain {
+    
+    
+   public func sayHello() {
+        
+        print("hello i am working fine ")
+    }
+    
+    public func sayHelloToWorld() {
+        
+        print("hello i am working fine for world are you going to work for me!!!")
+    }
+    
+}
